10fdf2032c31b60e905112e96cbb0311ec2b453e
Edmund Miller <edmund.a.miller@protonmail.com>
2021-05-19 12:27:20 -0500

    fix(geo_download): Cut out optparse dependency
    I'm too lazy to make a multi-package image for just one extra dependency

--- bin/GEO_download.R
+++ bin/GEO_download.R
@@ -1,14 +1,10 @@
 library(GEOquery)
-library(optparse)
 
-option_list <- list(
-    make_option(c("-d", "--geo_id" ), type="character", default='GSE166766' , metavar="string" , help="GEO Id"            )
-    make_option(c("-o", "--outdir" ), type="character", default='./'        , metavar="path"   , help="Output directory." ),
-)
+args = commandArgs(trailingOnly=TRUE)
 
-filePaths <- getGEOSuppFiles(opt$geo_id)
+filePaths <- getGEOSuppFiles(args[1])
 # TODO Turn these into their own nextflow process
-GEO_ID_path <- paste0("./", opt$geo_id, "/")
+GEO_ID_path <- paste0(args[2], args[1], "/")
 tarF <- list.files(path = GEO_ID_path, pattern = "*.tar", full.names = TRUE)
 untar(tarF, exdir = GEO_ID_path)
 gzipF <- list.files(path = GEO_ID_path, pattern = "*.gz", full.names = TRUE)
