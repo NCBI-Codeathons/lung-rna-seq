adacda1ef7f15fd9300683810c6223e1bcecf64a
YAN FANG <49136056+YanFangBio@users.noreply.github.com>
2021-05-19 15:16:22 -0500

    Update 10X_code.R

--- bin/10X_code.R
+++ bin/10X_code.R
@@ -347,74 +347,34 @@ install.packages("devtools")
 devtools::install_github('cole-trapnell-lab/leidenbase')
 #devtools::install_github('cole-trapnell-lab/monocle3')
 devtools::install_github('cole-trapnell-lab/monocle3',ref='develop')
+install.packages("remotes")
+remotes::install_github("satijalab/seurat-wrappers")
+library(SeuratWrappers)
 library(monocle3)
 library(dplyr)
 
 # convert the Seurat object to formmat used in monocle 3
 cds <- as.cell_data_set(sobj) # use seurat project sobj here
-cds <- cluster_cells(cds = sobj.cds, reduction_method = "UMAP")
-cds <- learn_graph(sobj.cds, use_partition = TRUE)
-cds <- order_cells(sobj.cds, reduction_method = "UMAP", root_cells = hsc)
 # Generate a cell_data_set from 10X output
 #cds <- load_mm_data(mat_path = matrix_file, 
 #                    feature_anno_path = gene_file , 
 #                    cell_anno_path =  barcodes_file)
  
-#Pre-process the data
-cds <- preprocess_cds(cds, num_dim = 100)
-#Reduce dimensionality and visualize the cells
-cds <- reduce_dimension(cds)
-rowData(cds)$gene_short_name <- rowData(cds)$V2
-#Clusterring
-cds = cluster_cells(cds)
-## Learn a graph
-cds <- learn_graph(cds)
-## Order cells
-cds <- order_cells(cds)
+#Trajectory calculation
+cds <- cluster_cells(cds = cds, reduction_method = "UMAP")
+cds <- learn_graph(cds, use_partition = TRUE)
+cds <- order_cells(cds, reduction_method = "UMAP")
 
-library(stringr)
-colData(cds)$sample =  str_sub(rownames(colData(cds)),-1,-1)
-colData(cds_subset)$sample =  str_sub(rownames(colData(cds_subset)),-1,-1)
-#Subset cells
-cds_subset <- choose_cells(cds)
-cds_subset <- reduce_dimension(cds_subset)
-#dentify genes that are differentially expressed in different subsets of
-# cells from this partition:
-pr_graph_test_res <- graph_test(cds_subset, neighbor_graph="knn", cores=8)
-pr_deg_ids <- row.names(subset(pr_graph_test_res, morans_I > 0.01 & q_value < 0.05))
-gene_module_df <- find_gene_modules(cds_subset[pr_deg_ids,], resolution=1e-3)
-plot_cells(cds_subset, genes=gene_module_df, 
-           show_trajectory_graph=FALSE, 
-           label_cell_groups=FALSE)
-cds_subset = cluster_cells(cds_subset)
-plot_cells(cds_subset, color_cells_by="cluster")
-
-#Trajectory
-cds_subset <- learn_graph(cds_subset)
-plot_cells(cds_subset,
-           color_cells_by = "cluster",
-           label_groups_by_cluster=FALSE,
-           label_leaves=FALSE,
-           label_branch_points=FALSE)
-#pseudotime
-cds_subset <- order_cells(cds_subset)                  
-plot_cells(cds_subset,
+#pseudotime plot                 
+plot_cells(cds,
            color_cells_by = "pseudotime",
            label_cell_groups=FALSE,
            label_leaves=FALSE,
            label_branch_points=FALSE,
            graph_label_size=1.5)
 
+#plot gene in pseudotime
 AFD_genes <- c("Ly6c2", "Mki67", "Pltp", "Arg1", "Adgre1")
-AFD_lineage_cds <- cds_subset[rowData(cds_subset)$gene_short_name %in% AFD_genes, ]
+AFD_lineage_cds <- cds[rowData(cds)$gene_short_name %in% AFD_genes, ]
 plot_genes_in_pseudotime(AFD_lineage_cds, min_expr=0.5)
 
-
-# find the genes that are differentially expressed on the different paths through the trajectory
-ciliated_cds_pr_test_res <- graph_test(cds_subset, neighbor_graph="principal_graph", cores=4)
-pr_deg_ids <- row.names(subset(ciliated_cds_pr_test_res, q_value < 0.05))
-a = subset(ciliated_cds_pr_test_res, q_value < 0.05)
-b = a[order(a$p_value), ]
-
-AFD_lineage_cds <- cds_subset[rowData(cds_subset)$gene_short_name %in% AFD_genes, ]
-plot_genes_in_pseudotime(AFD_lineage_cds, min_expr=0.5)
